0dd4c2d393bcf83eb8a86a93ec2e082a092251c0
Bryan Vestey <bryan@vestey.dev>
2020-06-21 11:03:53 -0700

    Updated zsh files

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,6 +4,10 @@
 export GOPATH=~/dev/go
 export PATH=$PATH:$GOPATH/bin
 
+go_test() {
+  go test $* | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/SKIP/s//$(printf "\033[34mSKIP\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | GREP_COLOR="01;33" egrep --color=always '\s*[a-zA-Z0-9\-_.]+[:][0-9]+[:]|^'
+}
+
 ###############################################################################
 ## user bin Exports                                                          ##
 ###############################################################################
