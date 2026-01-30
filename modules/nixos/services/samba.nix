37bee5db9c168abe048dfa8983595d426db7f304
Bryan Vestey <bryan@vestey.dev>
2026-01-29 17:42:28 -0800

    Update samba credentials location

--- modules/nixos/services/samba.nix
+++ modules/nixos/services/samba.nix
@@ -12,7 +12,7 @@
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000" ];
+      [ "${automount_opts},credentials=/home/bzvestey/dev/nix-configs/smb-secrets,uid=1000,gid=1000" ];
   };
 
   networking.firewall.extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
